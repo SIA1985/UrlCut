5e6b20970d5d313a77f977a7e6fe3b92d2a070e6
Илья Савунов <ilasavunov@gmail.com>
2025-04-23 23:08:27 +0300

    feat: начальная работа с postgres

---
+++ cmd/main.go
@@ -0,0 +1,15 @@
+package main
+
+import(
+	"UrlCut/internal/storage"
+	"UrlCut/internal/interfaces"
+)
+
+func tryStorage(s interfaces.Storage) {
+	s.Init()
+	s.GetFullUrl("test")
+}
+
+func main() {
+	tryStorage(&storage.PSQL{})
+}
--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,20 +1,64 @@
 package storage
 
 
-/*SQL-хранилище*/
+import(
+	"database/sql"
+	_ "github.com/lib/pq"
+	"log"
+	"encoding/json"
+	"os"
+	"fmt"
+)
 
-type SQL struct {
+
+/*Postgres SQL-хранилище*/
+type PConfig struct {
+	host		string		`json:'host'`
+	port		string 		`json:'port'`
+	user		string
+	password	string 
+	dbname		string	
+}
+
+type PSQL struct {
 	cache 		*Cache
+	db			*sql.DB
 }
 
-func (s *SQL) Init() {
-	s.cache = NewCache(1000)
+func (p *PSQL) Init() {
+	var err error
+	var conf PConfig
+
+	p.cache = NewCache(1000)
+
+	/*json config parsing by path fron ENV*/
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+    "password=%s dbname=%s sslmode=disable",
+    conf.host, conf.port, conf.user, conf.password, conf.dbname)
+
+	p.db, err = sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//todo: defer s.db.Close() -> функция деструктор?
 }
 
-func (s *SQL) GetFullUrl(cutUrl string) (fullUrl string, err error) {
+func (p *PSQL) GetFullUrl(cutUrl string) (fullUrl string, err error) {
+	if p.cache.Contains(fullUrl) {
+		fullUrl, err = p.cache.Get(cutUrl)
+		return
+	}
 
+	/*request sql data*/
+
+	return
 }
 
-func (s *SQL) StoreCutUrl(cutUrl string, fullUrl string) (err error) {
+func (p *PSQL) StoreCutUrl(cutUrl string, fullUrl string) (err error) {
+	p.cache.Add(cutUrl, fullUrl)
 
+	/*add to sql*/
+
+	return
 }
