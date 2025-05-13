73478a840a759f5613a9f0af085e83cb9b4bf2bf
Илья Савунов <ilasavunov@gmail.com>
2025-05-13 23:05:19 +0300

    feat: разбор агрументов кс

--- cmd/main.go
+++ cmd/main.go
@@ -6,6 +6,7 @@ import (
 	"UrlCut/internal/logic"
 	"UrlCut/internal/server"
 	"UrlCut/internal/storage"
+	"flag"
 	"log"
 
 	"fmt"
@@ -55,17 +56,30 @@ func tryCutter(c *cutter.Cutter) {
 func main() {
 	var err error
 
+	mode := flag.String("mode", "http", "Тип взаимодействия: http, terminal")
+	storageType := flag.String("storage_type", "postgres", "Тип хранилища: postgres")
+	storageConfigPath := flag.String("storage_config_path", "/home/ilia/Desktop/temki/gonya/UrlCut/configs/postgres.json", "Путь кофигурационному файлу для хранилища")
+	cacheSize := flag.Int("cache_size", 100, "Размер кэша недавних ссылок")
+	cutUrlLen := flag.Int("cut_url_len", 6, "")
+
+	httpAddr := flag.String("http_addr", "localhost:8090", "Адрес http-сервера")
+
+	flag.Parse()
+
 	var p *storage.PSQL
-	p, err = storage.NewPSQL(storage.WithCacheSize(5),
-		storage.WithPostgresCngPath("/home/ilia/Desktop/temki/gonya/UrlCut/configs/postgres.json"))
+	switch *storageType {
+	case "postgres":
+		p, err = storage.NewPSQL(storage.WithCacheSize(*cacheSize),
+			storage.WithPostgresCngPath(*storageConfigPath))
+
+	}
 	if err != nil {
 		log.Fatalf("Ошибка в создании объекта storage: %v", err)
-		return
 	}
 	// tryStorage(p)
 
 	var c *cutter.Cutter
-	c, err = cutter.NewCutter(6)
+	c, err = cutter.NewCutter(*cutUrlLen)
 	if err != nil {
 		log.Fatalf("Ошибка в создании объекта cutter: %v", err)
 		return
@@ -82,7 +96,14 @@ func main() {
 	// tryLogic(l)
 
 	var s interfaces.Server
-	s, err = server.NewTerminal(l)
+	switch *mode {
+	case "http":
+		s, err = server.NewHTTP(l, *httpAddr)
+
+	case "terminal":
+		s, err = server.NewTerminal(l)
+
+	}
 	if err != nil {
 		log.Fatalf("Ошибка в создании объекта server: %v", err)
 		return
